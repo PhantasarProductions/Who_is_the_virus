05212ba898bf68ac38a4ba863c8272219efe4314
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-06-14 22:57:36 +0200

    flow

--- WhoIsTheVirus.cpp
+++ WhoIsTheVirus.cpp
@@ -21,17 +21,30 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.06.13
+// Version: 22.06.14
 // EndLic
+
+#include <time.h>
 #include <QCol.hpp>
 #include <Platform.hpp>
+#include <TRandom.hpp>
 #include "bdate.hpp"
 #include "NameClass.hpp"
+#include "Session.hpp"
 #include "Users.hpp"
 
 using namespace std;
 using namespace Virus;
 using namespace TrickyUnits;
+
+bool AnotherSession() {
+	QCol->Write(qColor::Yellow, "Play another session?");
+	QCol->Write(qColor::Magenta, " <Y/N> ");
+	QCol->Color(qColor::Cyan,qColor::Black);	
+	string Antwoord;
+	cin >> Antwoord;
+	return Antwoord.size() && (Antwoord[0] == 'y' || Antwoord[0] == 'Y');	
+}
 
 int main(int c, char** args) {
 	//QCol = &ANSI; // Test purposes only!
@@ -41,12 +54,27 @@ int main(int c, char** args) {
 	QCol->Doing("Platform", Platform());
 	QCol->Doing("ColorDrv", QCol->Name);	
 	QCol->Write(qColor::White, "(c) Jeroen P. Broks, released under the terms of the GPL3\n\n");
+	NameClass::NewAll();
+	TRandSeed(time(NULL));
+	//auto r = TRand(20); cout << "R<" << r << ">\n";
 	if (User::Login(c, args)->Okay) {
+		do {
+			auto un{ User::GetCurrent()->MyName() };
+			Session Ses{ nullptr };
+			if (TSession::HasSession(un)) {
+				QCol->Doing("Restoring", "Session");
+				Ses = TSession::Load(un);
+			} else {
+				QCol->Doing("Creating", "Session");
+				Ses = TSession::Create();
+			}
+			if (Ses) Ses->Run();
 
+		} while (AnotherSession());
 	} else {
 		QCol->Error("Login failed");
 	}
 	QCol->Reset();
-	NameClass::NewAll();
+
 
 }
