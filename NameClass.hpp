6b142269f4d5448342a85b9f240be6d8c8b52f25
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-06-15 17:45:46 +0200

    vUpdate

--- NameClass.hpp
+++ NameClass.hpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.06.14
+// Version: 22.06.15
 // EndLic
 #pragma once
 #include <map>
